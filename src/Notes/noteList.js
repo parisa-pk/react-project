9310f099e209d582e4d12fe16e2237fb86e1ce6a
parisa <parisapichka1@gmail.com>
2023-01-07 02:07:46 -0800

    test8

--- src/Notes/noteList.js
+++ src/Notes/noteList.js
@@ -1,11 +1,12 @@
 import Note from "./Note"
 
 
-export default function NoteList({name , lastName , tel , email , gender , address , notes , setNotes}){
+export default function NoteList({name , lastName , tel , email , gender , address , notes }){
 
     return(
         <div>
-            <Note/>
+            {notes.map(()=><Note name={name} lastName={lastName} tel={tel} email={email} gender={gender} address={address} />)
+            }
         </div>
     )
 }
